cbdbf725795f3794205e65c05a9a1ae461577a92
BooSpot <boospotmanager@gmail.com>
2018-11-09 19:53:26 +0500

    initializing repo

---
+++ class-boo-settings-helper.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Boo Settings API helper class
+ *
+ * @version 1.0
+ *
+ * @author BooSpot | RaoAbid
+ * @link https://boospot.com Rao Abid
+ */
+if ( !class_exists( 'Boo_Settings_Helper' ) ):
+	class Boo_Settings_Helper {
+		
+	}
+endif;
